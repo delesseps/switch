b38bba9ecb5ff419915447fecea46151f547d361
Andrew Lessels <andrewlessels@gmail.com>
2017-06-09 17:13:12 +1000

    Fix demo page not showing component Add space and enter key support for toggling checked state Disable key behavior when component in disabled state

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -9,10 +9,17 @@ describe('rc-switch', () => {
     switcher = mount(<Switch />);
   });
 
-  it('works', () => {
+  it('initial checked state is false and changes to true on click', () => {
     expect(switcher.state().checked).toBe(false);
     switcher.simulate('click');
     expect(switcher.state().checked).toBe(true);
+  });
+
+  it('initial checked state is true and changes to false on click', () => {
+    const wrapper = mount(<Switch defaultChecked/>);
+    expect(wrapper.state().checked).toBe(true);
+    wrapper.simulate('click');
+    expect(wrapper.state().checked).toBe(false);
   });
 
   it('should support onClick', () => {
@@ -24,5 +31,11 @@ describe('rc-switch', () => {
     wrapper.simulate('click');
     expect(onClick).toBeCalledWith(false);
     expect(onClick.mock.calls.length).toBe(2);
+  });
+
+  it('should not toggle when clicked in a disabled state', () => {
+    const wrapper = mount(<Switch disabled defaultChecked/>);
+    wrapper.simulate('click');
+    expect(wrapper.state().checked).toBe(true);
   });
 });
